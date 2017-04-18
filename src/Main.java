4cbd4169235073475c2e0c735613a6c51a15e5ba
Tomasz Rydzewski <tomaszrydzewski92@gmail.com>
2017-04-18 21:34:07 +0200

    git append heuristic algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
 import org.jsoup.nodes.Document;
 import org.jsoup.Jsoup;
-
+import com.simontuffs.onejar.Boot;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        try {
+            Boot.main(new String[0]);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         Document doc;
+
+
+
+
+
+
 
         try {
             doc = Jsoup.connect("http://example.com")
