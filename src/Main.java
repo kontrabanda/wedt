51d132cedce504cd3bef875ad06edcb7df2767e9
Tomasz Rydzewski <tomaszrydzewski92@gmail.com>
2017-05-25 01:16:47 +0200

    git append heuristic algorithm wrapper

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
+import data.FileReader;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        HeuristicAlgorithmWrapper haw = new HeuristicAlgorithmWrapper();
-        haw.runHeuristic();
+        List<String> actions = Arrays.asList(args);
+
+
+        final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir);
+
+
+        if(actions.contains("heuristic")) {
+            runHeuristicAlgorithm();
+        }
+
+        if(actions.contains("localmaxs")) {
+            runLocalMaxsAlgorithm();
+        }
+    }
+
+    private static void runHeuristicAlgorithm() {
+        System.out.println("runHeuristicAlgorithm");
+        //HeuristicAlgorithmWrapper heuristicAlgorithmWrapper = new HeuristicAlgorithmWrapper();
+        //heuristicAlgorithmWrapper.runHeuristic();
+    }
+
+    private static void runLocalMaxsAlgorithm() {
+        System.out.println("runLocalMaxsAlgorithm");
+
     }
 }
