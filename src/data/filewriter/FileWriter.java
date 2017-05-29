2388fb26af9e8349315276b5cb427e112e40ebfa
Tomasz Rydzewski <tomaszrydzewski92@gmail.com>
2017-05-30 01:51:49 +0200

    FileWriter implementation

--- src/data/filewriter/FileWriter.java
+++ src/data/filewriter/FileWriter.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class FileWriter {
     public void writeData(FileWriterData fileWriterData) {
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(fileWriterData.path + "/" + fileWriterData.filename), "utf-8"))) {
+                new FileOutputStream(fileWriterData.path + "/" + fileWriterData.filename, true), "utf-8"))) {
             writer.write(fileWriterData.data);
         } catch(IOException e) {
             e.printStackTrace();
